51891861a664964a6eb3f8707a229be2b0ac6d72
vruaaan <brianp.2680@gmail.com>
2026-06-01 15:40:32 +0800

    edited firebase

--- bot/services/firebase.py
+++ bot/services/firebase.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_service_account")
+    cred = credentials.Certificate("firebase_service_account.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
